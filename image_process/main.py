0a15b8404a1e5f65378f7efa3d30f4f40117d0c2
Kate Yun <ggobugi27@gmail.com>
2018-07-18 18:05:07 -0400

    (ADD): Update gitignore to prevent pycache to push

--- image_process/main.py
+++ image_process/main.py
@@ -4,14 +4,10 @@ import sys
 import warnings
 
 warnings.filterwarnings('ignore')
-# arg1 = sys.argv[1] #value1, youtube url
-# arg2 = sys.argv[2] #value2, name of directory
-# arg3 = float(sys.argv[3]) #value3, fram capture interval in seconds
+arg1 = sys.argv[1] #value1, youtube url
+arg2 = sys.argv[2] #value2, name of directory
+arg3 = float(sys.argv[3]) #value3, fram capture interval in seconds
 
 def getImages(url, name, seconds) : 
 	fy.download(url, 'public/'+name)
 	return cf.captureFace('public/'+name + '.mkv', seconds)#assume video is mkv (Refactor later)
-
-# getImages(arg1, arg2, arg3)
-
-# getImages("https://www.youtube.com/watch?v=9jTo6hTZmiQ", 'test1', 30)#for test
