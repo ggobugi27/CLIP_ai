3821a6daecf023f0a529a885f00a182468872474
Kate Yun <ggobugi27@gmail.com>
2018-07-18 17:12:51 -0400

    (REFACTOR): Separate python_shell in separate file

--- web/routes/index.js
+++ web/routes/index.js
@@ -1,55 +1,26 @@
 const express = require('express');
-const router = express.Router();	
+const router = express.Router();
 const ImageClips = require('../ImageClips');
+const runPy = require('./runPy')
+const folderName = 'test2';
 
-// Running a Python script:
-var PythonShell = require('python-shell');
-let info = {}
-let runPy = (videoUrl, name = 'test1', interval=10) => {
-  var options = {
-    mode: 'text',
-    pythonPath: '/usr/local/bin/python3',
-    pythonOptions: ['-u'],
-    scriptPath: '/Users/kateyun/Desktop/Fullstack/Senior-Phase/Stackathon/CLIP_ai/image_process',
-    args: [videoUrl, name, interval]
-  };
-  let pyshell = new PythonShell('main.py', options)
-
-  pyshell.send(options); // path, args etc
-  pyshell.on('message', function (message) {
-    // received a message sent from the Python script (a simple "print" statement)
-    console.log(message);
-    if(message.startsWith('frame')) info[message[6]] = message.slice(7)
+const respondWithAllImages = (req, res, next) => {
+  let clips = ImageClips.clipImages(folderName);
+  clips.sort((a,b) => (a.info > b.info) ? 1 : ((b.info > a.info) ? -1 : 0)); 
+  res.render('index', {
+    title: 'CLIP.ai',
+    imageList: clips,
+    showForm: true,
+    info: info,
   });
-  pyshell.end(function (err,code,signal) {
-    if (err) throw err;
-    console.log('The exit code was: ' + code);
-    console.log('The exit signal was: ' + signal);
-    console.log('finished');
-    console.log('finished');
-  });
-}
-
-module.exports = () => {
-  // a reusable function
-  const respondWithAllImages = (req, res, next) => {
-    let clips = ImageClips.clipImages('test2');
-    clips.sort((a,b) => (a.info > b.info) ? 1 : ((b.info > a.info) ? -1 : 0)); 
-    // console.log(clips)
-    res.render('index', {
-      title: 'CLIP.ai',
-      imageList: clips,
-      showForm: true,
-      info: info,
-    });
-  };
-
-  router.get('/', respondWithAllImages);
-
-  router.post('/api/video', (req, res, next) => {
-    const currentUrl = req.body.videoUrl;
-    if (currentUrl) runPy(currentUrl, 'test2', 1)
-    res.redirect('/');
-  });
-  return router;
 };
+
+router.get('/', respondWithAllImages);
+
+router.post('/api/video', (req, res, next) => {
+  const currentUrl = req.body.videoUrl;
+  if (currentUrl) runPy(currentUrl, folderName, 1)
+  res.redirect('/');
+});
+
+module.exports = router;
